c9d66a8ad6517782a64df26d49c93c2f5a1768fb
Avanesyan Artem <grodex234@gmail.com>
2024-09-21 14:33:02 +0300

    add routes, header, footer, favicon and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import { MainPage } from './components/MainPage/MainPage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MainPage />,
+		errorElement: <>Упс...Ошибка</>,
+	},
+	{
+		path: '/projects/*',
+		element: <>проекты</>,
+		errorElement: <>Упс...Ошибка</>,
+	},
+]);
 
 function App() {
-	return <div className='app'></div>;
+	return (
+		<div className='app'>
+			<Header />
+			<RouterProvider router={router} />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
